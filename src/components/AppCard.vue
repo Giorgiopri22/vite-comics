2d3f35f9862c0ad513813f327dabff59f1eb76aa
Giorgiopri22 <giorgioprizzi@gmail.com>
2023-05-04 16:15:56 +0200

    Fine eser

---
+++ src/components/AppCard.vue
@@ -0,0 +1,49 @@
+<script>
+
+
+export default {
+    props: {
+        image: {
+            type: String,
+            required: true
+        },
+        text: {
+            type: String,
+            required: true
+        }
+    }
+}
+
+</script>
+
+<template>
+
+    <div class="card">
+        <figure>
+            <img :src="image" alt="">
+        </figure>
+        <div class="text-card">
+            {{ text }}
+        </div>
+    </div>
+
+</template>
+
+<style lang="scss" scoped> 
+
+.card{
+
+    img{
+        width: 180px;
+        aspect-ratio: 1;
+        object-fit: cover;
+        object-position: center;
+        margin-bottom: 18px;
+    }
+
+    .text-card{
+        text-transform: uppercase;
+    }
+}
+ 
+</style>
---
+++ src/components/AppCardPrice.vue
@@ -0,0 +1,41 @@
+<script>
+
+
+export default {
+    props: {
+        image: {
+            type: String,
+            required: true
+        },
+        text: {
+            type: String,
+            required: true
+        }
+    }
+}
+
+</script>
+
+<template>
+    
+    <div class="list-item-buy">
+        <img :src="image" alt="">
+        {{ text }}
+    </div>
+
+</template>
+
+<style lang="scss" scoped> 
+
+.list-item-buy {
+    display: flex;
+    align-items: center;
+    gap: 15px;
+    text-transform: uppercase;
+
+    img {
+        height: 50px;
+    }
+}
+ 
+</style>
